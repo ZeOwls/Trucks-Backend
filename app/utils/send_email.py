613afcefb76a8828c1feaec3991f6f8182fa5890
AhmdNassar <engahmdnassar@gmail.com>
2019-09-01 17:01:20 +0200

    fix mail with arabic username test

--- app/utils/send_email.py
+++ app/utils/send_email.py
@@ -19,12 +19,12 @@ def pass_mail(password,receiver_email,name):
         smtp.starttls()
         smtp.ehlo()
         smtp.login(email_user, email_pss)
-        print(name)
         subject = "Hello from trucks company"
-        # body = f"Hello {name},\n\nWelcome to trucks company,here is your password: {password}.\nkeep it safe and be sure to remember it"
-        body = "يا هلا يا هلا "
+        body = f"Hello {name},\n\nWelcome to trucks company,here is your password: {password}.\nkeep it safe and be sure to remember it"
+        # body = "يا هلا يا هلا "
         msg = f"Subject: {subject}\n\n{body}".encode('UTF-8')
-        smtp.sendmail(email_user, receiver_email, msg)
+        msg = MIMEText(msg, _charset="UTF-8")
+        smtp.sendmail(email_user, receiver_email, msg.as_string())
 
 
 def file_mail(filename, name):
