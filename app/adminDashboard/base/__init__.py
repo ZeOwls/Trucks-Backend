b6caeb03d63a3b21ae162f200cb80d11cc567306
AhmdNassar <engahmdnassar@gmail.com>
2019-09-02 13:41:43 +0200

    return insertData

--- app/adminDashboard/base/__init__.py
+++ app/adminDashboard/base/__init__.py
@@ -1,22 +1,4 @@
-from flask import Blueprint, redirect, url_for
-
-# for customer test
-test = Blueprint(
-    'test',
-    __name__,
-    url_prefix='/',
-    template_folder='templates',
-    static_folder='static'
-)
-
-
-@test.route('/')
-def base():
-    redirect(url_for('base_blueprint.route_default')
-             )
-
-
-# END == > for customer test
+from flask import Blueprint
 
 blueprint = Blueprint(
     'base_blueprint',
